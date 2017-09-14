46204585166d21a6f18aa72162fa60e25eacb7ae
xiefeng <xiefeng@MSI>
2017-09-14 18:13:37 +0800

    Add Two Numbers

---
+++ leetcode/src/main/java/Solution.java
@@ -0,0 +1,62 @@
+
+
+/**
+ * 
+ * @author xiefeng
+ *	Add Two Numbers
+ *	Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ *	Output: 7 -> 0 -> 8
+ *	注意while循环中c==1 时也要进行处理。
+ */
+public class Solution {
+	 public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+	     ListNode head = new ListNode(0);
+	     ListNode pre = head;
+	     ListNode ths;
+	     int c = 0;
+	     while(l1!=null||l2!=null||c==1){
+	    	 int a = 0;
+	    	 int b = 0;
+	    	 
+	    	 if(l1 != null){
+	    		 a = l1.val;
+	    		 l1 = l1.next;
+	    	 }
+	    	 if(l2 != null){
+	    		 b = l2.val;
+	    		 l2 = l2.next;
+	    	 }
+	    	 
+	    	 ths = new ListNode((a+b+c)%10);
+	    	 if(a+b+c>=10){
+	    		 c = 1;
+	    	 }else{
+	    		 c = 0;
+	    	 }
+	    	 pre.next = ths;
+	    	 pre = ths;
+	     }
+		 return head.next;
+	 }
+	 public static void print(ListNode l1) {
+		while(l1!=null){
+			System.out.println(l1.val);
+			l1=l1.next;
+		}
+			
+	}
+	 public static void main(String[] args) {
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(1);
+		ListNode node = new Solution().addTwoNumbers(l1, l2);
+		Solution.print(node);
+		
+	}
+	 
+	 static class ListNode {
+		    int val;
+		    ListNode next;
+		    ListNode(int x) { val = x; }
+		}
+}
+
